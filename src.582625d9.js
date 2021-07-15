90173be5c4b47ef1f0f4549974310a5ae7706f36
sasha87972 <82048515+sasha87972@users.noreply.github.com>
2021-07-15 13:56:57 +0000

    Deploying to gh-pages from @ sasha87972/team-project-demo@18cb3aae4e3c67faae1d60f2fce2fd54a093d4ee 🚀

--- src.582625d9.js
+++ src.582625d9.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-demo/src.a07a1751.js.map
+//# sourceMappingURL=/team-project-demo/src.582625d9.js.map
